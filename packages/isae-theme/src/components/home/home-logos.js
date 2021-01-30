53832309fc9de1d2b16b6c5ffe5ffd143f787ae4
moitorrijos <juanmtorrijos@gmail.com>
2021-01-30 15:09:19 -0500

    Agregar logos en el pie de página de inicio.

--- packages/isae-theme/src/components/home/home-logos.js
+++ packages/isae-theme/src/components/home/home-logos.js
@@ -1,18 +1,50 @@
 import React from 'react';
 import MainContainer from '../main-container';
-import { styled } from 'frontity';
+import Image from "@frontity/components/image";
+import { connect, styled } from 'frontity';
 
 const LogosContainer = styled.div`
   padding: 8rem 0;
 `;
 
-const HomeLogos = () => {
+const ImageGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(10, 1fr);
+  gap: 20px;
+
+  img {
+    mix-blend-mode: multiply;
+  }
+`;
+
+
+const HomeLogos = ({ state }) => {
+  const homeLogos = [
+    'redulac-rrd',
+    'auppa',
+    'rep-wermap',
+    'ninos',
+    'omep',
+    'latino-america',
+    'aualcpi',
+    'inca',
+    'consejo-de-rectores-de-panama',
+    'auprica'
+    ]
   return(
     <LogosContainer>
       <MainContainer>
+        <ImageGrid>
+          {homeLogos.map(logo => (
+            <Image
+              src={`${state.source.url}/wp-content/uploads/2021/01/${logo}.png`}
+              alt="REDULAC/RRD"
+            />
+          ))}
+        </ImageGrid>
       </MainContainer>
     </LogosContainer>
   )
 }
 
-export default HomeLogos;
+export default connect(HomeLogos);
